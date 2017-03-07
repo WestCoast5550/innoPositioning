bc467c397f56c3c144622702dcde9a0c2b57ff9d
kisa7 <nastya.shev9594@gmail.com>
2017-03-07 14:33:24 +0300

    Trajectory generator

--- pathMiner/generator.py
+++ pathMiner/generator.py
@@ -1,19 +1,33 @@
 import random
+from collections import namedtuple
 
 
-f = open('out.txt', 'w')
-destX = 400
-destY = 600
-startX = 0
-startY = 0
-n = 800.0
+f = open('originTrajectories.txt', 'r')
+Distance = namedtuple('Distance', ['x', 'y'])
+DistanceList = []
+dist = 0.0
 
+#file reading
+for i in range(0, 5):
+    x = []
+    y = []
+    for line in f:
+        if not line.strip():
+            break
+        else:
+            x1, y1 = line.split(' ')
+            x.append(float(x1))
+            y.append(float(y1))
+    DistanceList.append(Distance(x, y))
+
+f = open('testTrajectories.txt', 'w')
 #generates test data
-for i in range(800):
-	x = i * (destX - startX) / n
-	y = i * (destY - startY) / n
+for j in range(len(DistanceList)):
+    for k in range(0, 100):
+        for i in range(len(DistanceList[j].x)):
 
-	#adding noise
-	noiseX = random.uniform(-0.1, 0.1)
-	noiseY = random.uniform(-0.1, 0.1)
-	f.write(repr(x + noiseX) + ' ' + repr(y + noiseY) + '\n')
+            #adding noise
+            noiseX = random.uniform(-0.1, 0.1)
+            noiseY = random.uniform(-0.1, 0.1)
+            f.write(repr(DistanceList[j].x[i] + noiseX) + ' ' + repr(DistanceList[j].y[i] + noiseY) + '\n')
+        f.write('\n')
